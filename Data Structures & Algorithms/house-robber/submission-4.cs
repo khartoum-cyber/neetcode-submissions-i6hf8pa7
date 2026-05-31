2513fc24cfda79d6abc16eadc0f24eae2cd68cd4
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2026-05-31 21:53:24 +0200

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int Rob(int[] nums) 
+    {
+        if(nums.Length == 1)
+            return nums[0];
+
+        int[] arr = new int[nums.Length];
+
+        arr[0] = nums[0];
+        arr[1] = Math.Max(arr[0], nums[1]);
+
+        for(int i = 2; i < nums.Length; i++)
+        {
+            arr[i] = Math.Max(arr[i - 1], arr[i - 2] + nums[i]);
+        }
+
+        return arr[nums.Length - 1];
+    }
+}
