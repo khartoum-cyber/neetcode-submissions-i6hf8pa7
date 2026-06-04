21f261fb31c8219df44db4726c185a2f6780649c
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2026-06-04 21:19:02 +0200

    Add: matrixBFS - submission-7

---
+++ Data Structures & Algorithms/matrixBFS/submission-7.cs
@@ -0,0 +1,46 @@
+public class Solution 
+{
+    public int ShortestPath(int[][] grid) 
+    {
+        int ROWS = grid.Length;
+        int COLS = grid[0].Length;
+
+        if(grid[0][0] == 1 || grid[ROWS - 1][COLS - 1] == 1)
+            return -1;
+        
+        int[][] dirs = new int[][]
+        {
+            new int[] { 1, 0 },
+            new int[] { -1, 0 },
+            new int[] { 0, 1 },
+            new int[] { 0, -1 }
+        };
+
+        Queue<(int r, int c, int d)> q = new();
+
+        grid[0][0] = 1;
+        q.Enqueue((0, 0, 0));
+
+        while(q.Count > 0)
+        {
+            var (r, c, d) = q.Dequeue();
+
+            if(r == ROWS - 1 && c == COLS - 1)
+                return d;
+            
+            foreach(var dir in dirs)
+            {
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+
+                if(nr >= 0 && nr < ROWS && nc >= 0 && nc < COLS && grid[nr][nc] == 0)
+                {
+                    grid[nr][nc] = 1;
+                    q.Enqueue((nr, nc, d + 1));
+                }
+            }
+        }
+
+        return -1;
+    }
+}
