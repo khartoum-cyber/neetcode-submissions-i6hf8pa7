b01450fda35015a8af4acb79279610d3f6262932
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2026-06-06 23:18:01 +0200

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.cs
@@ -0,0 +1,31 @@
+public class Solution 
+{
+    public int Rob(int[] nums) 
+    {
+        if (nums.Length == 0) 
+            return 0;
+        if (nums.Length == 1) 
+            return nums[0];
+
+        return Math.Max(Helper(nums[1..]), Helper(nums[..^1]));
+    }
+
+    private int Helper(int[] nums)
+    {
+        if (nums.Length == 0) 
+            return 0;
+        if (nums.Length == 1) 
+            return nums[0];
+
+        int[] dp = new int[nums.Length];
+        dp[0] = nums[0];
+        dp[1] = Math.Max(nums[0], nums[1]);
+
+        for(int i = 2; i < nums.Length; i++)
+        {
+            dp[i] = Math.Max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+
+        return dp[^1];
+    }
+}
