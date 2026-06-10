0526597529653528e8f3d3633add4da53c5642f3
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2026-06-10 13:59:03 +0200

    Add: word-break - submission-5

---
+++ Data Structures & Algorithms/word-break/submission-5.cs
@@ -0,0 +1,30 @@
+public class Solution 
+{
+    public bool WordBreak(string s, List<string> wordDict) 
+    {
+        Dictionary<int,bool> memo = new Dictionary<int, bool> { { s.Length, true } };
+
+        return DFS(0, s, wordDict, memo);
+    }
+
+    private bool DFS(int i, string s, List<string> wordDict, Dictionary<int,bool> memo)
+    {
+        if(memo.ContainsKey(i))
+            return memo[i];
+
+        foreach(var word in wordDict)
+        {            
+            if(i + word.Length <= s.Length && word == s.Substring(i, word.Length))
+            {
+                if(DFS(i + word.Length, s, wordDict, memo))
+                {
+                    memo[i] = true;
+                    return true;
+                }
+            }
+        }
+        
+        memo[i] = false;
+        return false;
+    }
+}
