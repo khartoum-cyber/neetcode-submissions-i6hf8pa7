1082a0a7dfb3fd2e3dc6bc45ca155070734c26e4
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2026-06-11 19:26:11 +0200

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int HammingWeight(uint n) 
+    {
+        int res = 0;
+
+        for(int i = 0; i < 32; i++)
+        {
+            var mask = 1 << i;
+
+            if((mask & n) != 0)
+                res++;
+        }
+
+        return res;
+    }
+}
