1b8b9d4a5bbe5f8f79942f9ca69423ea4ab5d5a3
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2026-06-23 10:36:27 +0200

    Add: maximum-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-subarray/submission-3.cs
@@ -0,0 +1,16 @@
+public class Solution 
+{
+    public int MaxSubArray(int[] nums) 
+    {
+        int currSum = nums[0];
+        int maxSum = nums[0];
+
+        for(int i = 1; i < nums.Length; i++)
+        {
+            currSum = Math.Max(nums[i], currSum + nums[i]);
+            maxSum = Math.Max(maxSum, currSum);
+        }
+
+        return maxSum;
+    }
+}
