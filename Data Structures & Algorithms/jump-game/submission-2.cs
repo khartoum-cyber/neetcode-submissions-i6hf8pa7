1a4e5bb0fc66fa4e4136b14f3a83b20364c88f83
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2026-06-24 09:22:27 +0200

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.cs
@@ -0,0 +1,31 @@
+public class Solution 
+{
+    public bool CanJump(int[] nums) 
+    {
+        Dictionary<int,bool> memo = new();
+        return DFS(0, nums, memo);
+    }
+
+    private bool DFS(int i, int[] nums, Dictionary<int,bool> memo)
+    {
+        if(i >= nums.Length - 1)
+            return true;
+        
+        if(memo.ContainsKey(i))
+            return memo[i];
+        
+        int end = Math.Min(nums.Length - 1, i + nums[i]);
+
+        for(int j = end; j > i; j--)
+        {
+            if(DFS(j, nums, memo))
+            {
+                memo[i] = true;
+                return true;
+            }
+        }
+
+        memo[i] = false;
+        return false;
+    }
+}
