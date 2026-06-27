52f34b9710ce9e4b99fa6256e7d2bf14ab3ee5a9
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2026-06-27 08:55:48 +0200

    Add: non-overlapping-intervals - submission-2

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-2.cs
@@ -0,0 +1,23 @@
+public class Solution 
+{
+    public int EraseOverlapIntervals(int[][] intervals) 
+    {
+        Array.Sort(intervals, (a, b) => a[1].CompareTo(b[1]));
+
+        int res = 0;
+        int prevEnd = intervals[0][1];
+
+        for(int i = 1; i < intervals.Length; i++)
+        {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+
+            if(start < prevEnd)
+                res++;
+            else
+                prevEnd = end;
+        }
+
+        return res;
+    }
+}
