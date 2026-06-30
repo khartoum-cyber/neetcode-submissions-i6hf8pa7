cab4072e51d29828a7514949a57d9fd1aacd775e
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2026-06-30 10:39:11 +0200

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.cs
@@ -0,0 +1,29 @@
+public class Solution 
+{
+    public int[][] Insert(int[][] intervals, int[] newInterval) 
+    {
+        var res = new List<int[]>();
+
+        for(int i = 0; i < intervals.Length; i++)
+        {
+            if(newInterval[1] < intervals[i][0])
+            {
+                res.Add(newInterval);
+                res.AddRange(intervals.AsEnumerable().Skip(i).ToArray());
+                return res.ToArray();
+            }
+            else if(newInterval[0] > intervals[i][1])
+            {
+                res.Add(intervals[i]);
+            }
+            else
+            {
+                newInterval[0] = Math.Min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.Max(newInterval[1], intervals[i][1]);
+            }
+        }
+
+        res.Add(newInterval);
+        return res.ToArray();
+    }
+}
