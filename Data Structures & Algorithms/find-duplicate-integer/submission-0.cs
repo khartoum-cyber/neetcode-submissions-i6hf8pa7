4d8f11a127c8a844672f28f933c6100323aeaa57
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2026-08-10 15:33:08 +0200

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.cs
@@ -0,0 +1,17 @@
+public class Solution 
+{
+    public int FindDuplicate(int[] nums) 
+    {
+        Array.Sort(nums);
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            for(int j = i + 1; j < nums.Length; j++)
+            {
+                if(nums[i] == nums[j])
+                    return nums[i];
+            }
+        }
+        return -1;
+    }
+}
