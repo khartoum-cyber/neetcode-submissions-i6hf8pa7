32cfefeb6b3441d188c35441a10f9cfdd995ea7e
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2026-08-31 13:30:37 +0200

    Add: continuous-subarray-sum - submission-7

---
+++ Data Structures & Algorithms/continuous-subarray-sum/submission-7.cs
@@ -0,0 +1,19 @@
+public class Solution 
+{
+    public bool CheckSubarraySum(int[] nums, int k) 
+    {
+        for(int i = 0; i < nums.Length; i++)
+        {
+            int curSum = nums[i];
+            for(int j = i + 1; j < nums.Length; j++)
+            {
+                curSum += nums[j];
+
+                if(curSum % k == 0 && j > i)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}
