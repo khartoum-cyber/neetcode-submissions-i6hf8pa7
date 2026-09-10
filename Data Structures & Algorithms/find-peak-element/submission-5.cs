30d05e3c07acae4ad7b393d7f547721aad2b66d9
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2026-09-10 15:24:12 +0200

    Add: find-peak-element - submission-5

---
+++ Data Structures & Algorithms/find-peak-element/submission-5.cs
@@ -0,0 +1,20 @@
+public class Solution 
+{
+    public int FindPeakElement(int[] nums) 
+    {
+        int l = 0;
+        int r = nums.Length - 1;
+
+        while(l < r)
+        {
+            int mid = l + (r - l) / 2;
+
+            if(nums[mid] < nums[mid + 1])
+                l = mid + 1;
+            else
+                r = mid;
+        }
+
+        return l;
+    }
+}
