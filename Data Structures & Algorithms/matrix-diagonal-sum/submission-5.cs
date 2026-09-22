d3606befe0132f225406d935bbb4787036f16823
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2026-09-22 10:40:34 +0200

    Add: matrix-diagonal-sum - submission-5

---
+++ Data Structures & Algorithms/matrix-diagonal-sum/submission-5.cs
@@ -0,0 +1,19 @@
+public class Solution 
+{
+    public int DiagonalSum(int[][] mat) 
+    {
+        int n = mat.Length;
+        int res = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            res += mat[i][i];
+            res += mat[i][n - i - 1];
+        }
+
+        if(n % 2 == 1)
+            res -= mat[n / 2][n / 2];
+
+        return res;
+    }
+}
